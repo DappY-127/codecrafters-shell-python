7b3ddc4ce120f6be88270b616082554754529d18
Paul <paul.mshtk.707@gmail.com>
2024-12-20 13:10:58 +0200

    add type builtin

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import sys
 
 
+BUILTIN_COMMANDS = {"exit", "echo", "type"}
+
 def main():
     while True:
         try:
@@ -20,6 +22,18 @@ def main():
             elif command.startswith('echo'):
                 command, _, message = command.partition(" ")
                 print(message)
+
+            elif command.startswith('type'):
+                command, _, command_name = command.partition(" ")
+                if not command_name.strip():
+                    print("type: argument required")
+                    break
+
+                for command_name in command_name.split():
+                    if command_name in BUILTIN_COMMANDS:
+                        print(f'{command_name} is a shell builtin')
+                    else:
+                        print(f'{command_name}: not found')
 
             else:
                 print(f"{command}: command not found")
