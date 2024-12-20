713d34f9f2a0fc0f759339c3ea43f77bc08abc99
Paul <paul.mshtk.707@gmail.com>
2024-12-20 12:42:34 +0200

    add exit builtin

--- app/main.py
+++ app/main.py
@@ -8,11 +8,20 @@ def main():
             sys.stdout.flush()
             command = input().strip()
 
+            if command.startswith('exit'):
+                command, _, status_code = command.partition(" ")
+                status_code = status_code.strip()
+                if status_code.isdigit():
+                    sys.exit(int(status_code))
+                else:
+                    print("exit: numeric argument required")
+                    sys.exit(0)
+
             if command:
                 print(f"{command}: command not found")
         except (KeyboardInterrupt, EOFError):
             print("\nExiting...")
-            break
+            sys.exit(0)
 
 
 if __name__ == "__main__":
